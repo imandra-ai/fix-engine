f3a9f37da62444a1625bd0150ef18874a6ed01c9
Denis Ignatovich <denis@aestheticintegration.com>
2017-03-13 16:26:33 +0000

    [feat] all modules in doc files

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -14,6 +14,13 @@ let () =
   Pkg.describe "fix-engine" @@ fun c ->
   let examples = Conf.value c examples in
   Ok [ Pkg.mllib ~api:["fix_engine"] "src/fix_engine.mllib"
+       ; Pkg.mllib ~api:["datetime"
+                        ; "full_messages"
+                        ; "full_admin_messages"
+                        ; "full_app_messages"
+                        ; "full_session_core"
+                        ; "basic_types"
+                        ; "full_fix_fields"] "src-protocol/fix_protocol.mllib"
 (*     ; Pkg.test ~dir:"src" "test/main" *)
      ; Pkg.bin ~cond:examples "src-examples/fix_examples"
      ]
