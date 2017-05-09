13f56c2801538aa523d80964f6ee18bc44a4ae34
Kostya <kkanishev@gmail.com>
2017-05-09 09:03:07 +0200

    update pkg.ml and vgs.ml

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -3,45 +3,31 @@
 #require "topkg"
 open Topkg
 
-let examples =
+let vgs =
   Conf.key
-    "build-examples"
+    "build-vgs"
     Conf.bool
-    ~doc:"Build the example programs"
+    ~doc:"Build a reduced VGs version and the example programs"
     ~absent:false
 
 let () =
   Pkg.describe "fix-engine" @@ fun c ->
-  let examples = Conf.value c examples in Ok
+  let vgs = Conf.value c vgs in Ok
   [  
       Pkg.mllib ~api: [ "base_types"
                       ; "datetime"
                       ; "imandra_utils"
-                      ; "numeric" ] "src-core/core.mllib"
+                      ; "numeric" 
+                      ] "src-core/core.mllib"
 
-    ; Pkg.mllib ~api: [ "full_app_enums"
-                      ; "full_app_tags"
-                      ; "full_app_messages"
-                      ] "src-protocol-exts/protocol_exts.mllib"
+    ; Pkg.mllib ~api: [ "base_types_json"
+                      ; "datetime_json" 
+                      ] "src-core-pp/core_pp.mllib"
 
     ; Pkg.mllib ~api: [ "full_messages"
                       ; "full_admin_messages"
                       ; "full_session_core"
                       ] "src-protocol/protocol.mllib"
-
-    ; Pkg.mllib ~api: [ "fix_engine" ] "src/fix_engine.mllib"
-
-
-    ; Pkg.mllib ~api: [ "base_types_json"
-                      ; "datetime_json" ] "src-core-pp/core_pp.mllib"
-
-    ; Pkg.mllib ~api: [ "full_app_enums_json"
-                      ; "full_app_tags_json"
-                      ; "full_app_messages_json" 
-                      ; "parse_app_enums"
-                      ; "parse_app_tags"
-                      ; "parse_app_messages" 
-                      ] "src-protocol-exts-pp/protocol_exts_pp.mllib" 
 
     ; Pkg.mllib ~api: [ "fix_engine_json"
                       ; "full_admin_enums_json"
@@ -54,10 +40,41 @@ let () =
                       ; "parse_admin_tags"
                       ] "src-protocol-pp/protocol_pp.mllib"
 
+    ; Pkg.mllib ~api: [ "fix_engine" ] "src/fix_engine.mllib"
+
+    ; Pkg.mllib ~cond: (not vgs)
+                ~api: [ "full_app_enums"
+                      ; "full_app_tags"
+                      ; "full_app_messages"
+                      ] "src-protocol-exts/protocol_exts.mllib"
+
+    ; Pkg.mllib ~cond: (not vgs)
+                ~api: [ "full_app_enums_json"
+                      ; "full_app_tags_json"
+                      ; "full_app_messages_json" 
+                      ; "parse_app_enums"
+                      ; "parse_app_tags"
+                      ; "parse_app_messages" 
+                      ] "src-protocol-exts-pp/protocol_exts_pp.mllib" 
+
+    ; Pkg.mllib ~cond: vgs
+                ~api: [ "full_app_enums"
+                      ; "full_app_tags"
+                      ; "full_app_messages"
+                      ] "src-protocol-exts-vg/protocol_exts.mllib"
+
+    ; Pkg.mllib ~cond: vgs
+                ~api: [ "full_app_enums_json"
+                      ; "full_app_tags_json"
+                      ; "full_app_messages_json" 
+                      ; "parse_app_enums"
+                      ; "parse_app_tags"
+                      ; "parse_app_messages" 
+                      ] "src-protocol-exts-pp-vg/protocol_exts_pp.mllib" 
+
     ; Pkg.bin "src-examples/wire_to_json"
-(*
-    ; Pkg.bin ~cond:examples "src-examples/example_runner"
-    *)
+
+    ; Pkg.bin ~cond:vgs "src-examples/example_runner"
 
     ; Pkg.doc "doc/all_modules.docdir/all_modules.dot"
 
