35fa86cc0147dc090024fe58a9fcaec8b63f9336
Grant Passmore <grant@Grants-iMac.local>
2017-05-19 18:00:42 -0500

    feat(vgs/decomp): Toplevel decomposition driver

---
+++ vgs/top.ml
@@ -0,0 +1,26 @@
+(**
+
+    Aesthetic Integration Limited
+    Copyright (c) 2014 - 2017
+
+    Implementation of the FIX 4.4 protocol.
+
+    Toplevel model load file for Imandra.
+*)
+
+:l src-core/datetime.ml
+:l src-core/numeric.ml
+:l src-core/base_types.ml
+
+:l src-protocol/full_admin_tags.ml
+:l src-protocol-exts/full_app_tags.ml
+:l src-protocol/full_message_tags.ml
+
+:l src-protocol/full_admin_enums.ml
+:l src-protocol/full_admin_messages.ml
+:l src-protocol-exts/full_app_enums.ml
+:l src-protocol-exts/full_app_records.ml
+:l src-protocol-exts/full_app_messages.ml
+
+:l src-protocol/full_messages.ml
+:l src/fix_engine.ml
