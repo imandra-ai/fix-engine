0dd04acca524f0dca7be0d41c6513b7a715203b3
Kostya <kkanishev@gmail.com>
2017-06-28 10:07:11 +0200

    Empty_string support in parsing code. Closes #33

--- src-core-pp/string_utils.ml
+++ src-core-pp/string_utils.ml
@@ -13,11 +13,19 @@ open Base_types;;
 let fix_to_str_hashtbl : (fix_string, string) Hashtbl.t = Hashtbl.create 10;;
 let str_to_fix_hashtbl : (string, fix_string) Hashtbl.t = Hashtbl.create 10;;
 
+let add_model_string ( fixstr, rawstr : fix_string * string ) : unit = 
+    Hashtbl.add str_to_fix_hashtbl rawstr fixstr;
+    Hashtbl.add fix_to_str_hashtbl fixstr rawstr
+;;
+
+add_model_string ( Model_string 1, "Empty");;
+
 let fix_string_to_string fixstr = 
     if not (Hashtbl.mem fix_to_str_hashtbl fixstr) then
         ( match fixstr with
-            | Model_string hash -> failwith ( "Unrecognized model string \"" ^ string_of_int hash ^ "\".")
-            | Admin_string hash -> failwith ( "Unrecognized admin string \"" ^ string_of_int hash ^ "\".")
+            | Model_string hash ->  failwith ( "Unrecognized model string \"" ^ string_of_int hash ^ "\".")
+            | Admin_string hash ->  failwith ( "Unrecognized admin string \"" ^ string_of_int hash ^ "\".")
+            | Empty_string -> ( add_model_string ( Empty_string, "" ); "")
         )
     else Hashtbl.find fix_to_str_hashtbl fixstr;;
 
