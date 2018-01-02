4ddce8ba2bd8d7789dac7eff8089f7c740f4a17a
Kostya <kkanishev@gmail.com>
2018-01-02 21:52:12 +0100

    Client code from test manager

---
+++ src-simulation/client.ml
@@ -0,0 +1,139 @@
+(** FIX 4.4 engine client implementation. *)
+(***
+    
+    Aesthetic Integration Limited
+    Copyright (c) 2014 - 2017
+
+    client.ml
+    
+*)
+
+let (>>=) = Lwt.(>>=);;
+
+let ctime = Fix_connection.get_current_utctimstamp;;
+
+let zmq_publish socket msg =
+    let msg = Model_messages_json.json_of_model_msg msg in
+    let msg = Yojson.to_string msg in
+    Lwt_zmq.Socket.send socket msg  
+;;
+
+let rec zmq_rep_loop (socket, mailbox) =
+    Lwt_zmq.Socket.(recv socket) >>= fun msg ->
+    if msg = "LOGOUT" then 
+        Lwt_mvar.put mailbox Fix_global_state.Terminate  >>= fun () ->
+        Lwt_zmq.Socket.(send socket "ACKED")
+    else if msg = "STATUS" then
+        Lwt_zmq.Socket.(send socket "UP") >>= fun () ->
+        zmq_rep_loop (socket, mailbox) 
+    else
+    let action =
+        try  
+            Json_to_action.json_to_fix_action (Yojson.Basic.from_string msg)
+        with 
+            ex -> None
+        in
+    match action with 
+    | None -> (
+        Lwt_zmq.Socket.(send socket "REJECTED") >>= fun () ->
+        zmq_rep_loop (socket, mailbox) )   
+    | Some action -> (
+        Lwt_mvar.put mailbox (Fix_global_state.ModelAction action) >>= fun () ->
+        Lwt_zmq.Socket.(send socket "ACKED") >>= fun () ->
+        zmq_rep_loop (socket, mailbox) )
+;;
+
+let f config (zmqrep, zmqpub) (inch, outch) =
+    let close_channels () = 
+        Lwt_io.printl "Connection closed, shutting down." >>= fun () ->
+        Lwt_io.close inch >>= fun () ->
+        Lwt_io.close outch
+        in
+    let open Fix_connection in
+    let config = { config with pub_callback = Some (zmq_publish zmqpub) } in
+    let connection = create config (inch, outch) in 
+    let mailbox = get_input_mailbox connection in  
+    let initmsg = Fix_global_state.Internal_Message ( create_session_msg connection ) in
+    Lwt.catch ( fun () ->
+        Lwt_mvar.put mailbox initmsg >>= fun () -> 
+        Lwt.choose [
+            get_heartbeat_thread connection;
+            get_fix_thread connection;
+            zmq_rep_loop (zmqrep, mailbox)
+        ]
+    ) ( fun _ -> close_channels () )
+;;
+
+
+let run_client fixhost fixport config zmqpub zmqrep = 
+    (* Bringing up a ZMQ sockets *)
+    let zmqcontext = ZMQ.Context.create () in
+    let zmqpubsocket = ZMQ.Socket.(create zmqcontext pub) in
+    let () = ZMQ.Socket.bind zmqpubsocket zmqpub in
+    let zmqpubsocket = Lwt_zmq.Socket.of_socket zmqpubsocket in
+    let zmqrepsocket = ZMQ.Socket.(create zmqcontext rep) in
+    let () = ZMQ.Socket.bind zmqrepsocket zmqrep in    
+    let zmqrepsocket = Lwt_zmq.Socket.of_socket zmqrepsocket in
+    (* Creating the address *)
+    let addr = Unix.( (gethostbyname fixhost).h_addr_list.(0) ) in      
+    let addr = Unix.( ADDR_INET( addr , fixport ) ) in
+    (**)
+    let () = [
+        "(*********                   FIX Engine Client                           *********)";
+        "(*********  (c)Copyright Aesthetic Integration Limited., 2014 - 2017     *********)\n";
+        Printf.sprintf " - FIX client connecting to %s:%d" fixhost fixport;
+        Printf.sprintf " - FIX session %s -> %s" config.Fix_connection.comp_id config.Fix_connection.target_id;
+        Printf.sprintf " - Internal messages are published on ZMQ socket %s" zmqpub;
+        Printf.sprintf " - Model actions are received on ZMQ socket %s" zmqrep;
+        "\n(*********************************************************************************)\n";
+        ] |> String.concat "\n" |> print_endline in
+    let client_thread = Lwt_io.with_connection addr ( f config (zmqrepsocket, zmqpubsocket) ) in
+    Lwt_main.run client_thread
+;;
+
+let mkconfig compid hostid targetid reset =
+    Fix_connection.{
+        comp_id = compid;
+        host_id = Some hostid;
+        target_id = targetid;
+        pub_callback = None;
+        reset_seq = reset;
+        timer = 1.0;
+    }
+;;
+
+let () =
+    let open Cmdliner in
+    let fixhost = let doc = "FIX host address" in
+        Arg.(value & opt string "localhost" & info ["fix-host"] ~docv:"FIXHOST" ~doc)
+        in
+    let fixport = let doc = "Port for the FIX connection" in 
+        Arg.(value & opt int 9880 & info ["fix-port"] ~docv:"FIXPORT" ~doc)
+        in
+    let compid = let doc = "FIX Client ID" in
+        Arg.(value & opt string "IMANDRA" & info ["compid"] ~docv:"COMPID" ~doc)
+        in
+    let hostid = let doc = "Host ID (will be sent as SenderLocationID<142>)" in
+        Arg.(value & opt string "LOGIN" & info ["hostid"] ~docv:"HOSTID" ~doc)
+        in
+    let targetid = let doc = "FIX Target ID" in
+        Arg.(value & opt string "TARGET" & info ["targetid"] ~docv:"TARGETID" ~doc)
+        in
+    let zmqpub =
+        let doc = "ZMQ PUB socket adress" in
+        Arg.(value & opt string "tcp://*:5000" & info ["zmq-pub"] ~docv:"ZMQPUBADDR" ~doc)
+        in
+    let zmqrep =
+        let doc = "ZMQ REP socket adress" in
+        Arg.(value & opt string "tcp://*:5001" & info ["zmq-rep"] ~docv:"ZMQREPADDR" ~doc)
+        in
+    let reset = 
+        let doc = "Whether we shall send ResetSeqNum with the logon" in
+        Arg.(value & flag & info ["reset-seq"] ~doc)
+        in        
+    let info = let doc = "FIX engine client, publishing all internal messages on ZMQ socket." in
+        Term.info "server" ~version:"%%VERSION%%" ~doc ~exits:Term.default_exits 
+        in
+    let config = Term.(const mkconfig $ compid $ hostid $ targetid $ reset ) in
+    Term.exit @@ Term.eval (Term. (const run_client $ fixhost $ fixport $ config $ zmqpub $ zmqrep ), info)
+;;
---
+++ src-simulation/fix_connection.ml
@@ -0,0 +1,168 @@
+type config = {
+  comp_id        : string;
+  host_id        : string option;
+  target_id      : string;
+  timer          : float;
+  reset_seq      : bool;  
+  pub_callback   : (Model_messages.model_msg -> unit Lwt.t) option;
+}
+
+type t = {
+  heartbeat : unit Lwt.t;
+  fix       : unit Lwt.t;
+  mailbox   : Fix_global_state.incoming_event Lwt_mvar.t;
+  initmsg   : Fix_engine_state.fix_engine_int_inc_msg;
+}
+
+let get_heartbeat_thread t = t.heartbeat
+let get_fix_thread       t = t.fix 
+let get_input_mailbox    t = t.mailbox 
+let create_session_msg   t = t.initmsg
+
+
+let get_current_utctimstamp () =    
+  let open Datetime in
+  let dtime = Unix.gettimeofday () in 
+  let tm = Unix.gmtime dtime in
+  let msec = int_of_float (1000. *. (dtime -. floor dtime)) in
+  {  utc_timestamp_year   = tm.Unix.tm_year + 1900
+  ;  utc_timestamp_month  = tm.Unix.tm_mon + 1
+  ;  utc_timestamp_day    = tm.Unix.tm_mday
+  ;  utc_timestamp_hour   = tm.Unix.tm_hour
+  ;  utc_timestamp_minute = tm.Unix.tm_min
+  ;  utc_timestamp_second = tm.Unix.tm_sec
+  ;  utc_timestamp_millisec = Some (msec)
+}
+
+let read_int file =
+  try
+    let chn = open_in file in
+    let num = input_line chn in
+    let () = close_in chn in
+    int_of_string num
+  with
+    exn -> 0
+;;
+
+let read_seqns dir =
+  if not(Sys.file_exists dir) then 
+    let () = Unix.(mkdir dir 0o775) in (0,0)  
+  else
+    let seqin  = read_int Filename.(concat dir "seqin" ) in
+    let seqout = read_int Filename.(concat dir "seqout") in
+    (seqin, seqout)
+;;
+
+let make_engine_state session_dir config =
+  let s2f = String_utils.string_to_fix_string in
+  let fe_comp_id            = s2f config.comp_id in 
+  let fe_target_comp_id     = s2f config.target_id in    
+  let fe_sender_location_id = 
+    match config.host_id with None -> None 
+    | Some host -> Some ( s2f host ) in  
+    let open Fix_engine_state in
+  let inseq,outseq = read_seqns session_dir in
+  { init_fix_engine_state with
+    fe_comp_id;
+    fe_sender_location_id;
+    fe_target_comp_id;
+    fe_curr_time = get_current_utctimstamp ();
+    fe_max_num_logons_sent = 10;
+    fe_application_up = true;
+    incoming_seq_num = inseq;
+    outgoing_seq_num = outseq;    
+}
+ 
+let (>>=) = Lwt.(>>=)
+
+(** Timechange thread  *)
+
+let do_timechange mailbox =
+  let time = get_current_utctimstamp () in
+  let timechange = Fix_engine_state.IncIntMsg_TimeChange time in
+  let timechange = Fix_global_state.Internal_Message timechange in
+  Lwt_mvar.put mailbox timechange
+;;
+
+(** Heartbeat thread  *)
+
+let rec heartbeat_thread mailbox =
+  let open Fix_engine_state in
+  Lwt_unix.sleep (1.0) >>= fun () -> 
+  do_timechange mailbox >>= fun () -> 
+  heartbeat_thread mailbox
+;;
+
+
+let write_seqn session_dir fname msg =
+  let open Full_messages in
+  if msg == Garbled then Lwt.return_unit else
+  let seqn = match msg with 
+    | ValidMsg data -> data.full_msg_header.h_msg_seq_num
+    | SessionRejectedMsg data -> data.srej_msg_msg_seq_num                       
+    | BusinessRejectedMsg  data -> data.brej_msg_ref_seq_num                          
+    | Garbled -> 0
+    in
+    let filename = Filename.(concat session_dir fname) in 
+    Lwt_io.(open_file ~mode:output filename) >>= fun fch ->
+    Lwt_io.write fch (string_of_int seqn) >>= fun () ->
+    Lwt_io.close fch
+;;
+
+let log_flags = Unix.[O_WRONLY; O_APPEND; O_CREAT];;
+
+let fix_thread session_dir inch mailbox = 
+  let filename = Filename.(concat session_dir "fix.log") in 
+  Message_stream.from_channel ~verbose:true inch
+  |> Lwt_stream.iter_s ( fun msg ->
+    let str = msg |> List.map (fun (k,v) -> k ^ "=" ^ v)
+                  |> String.concat "|" 
+                  |> fun s -> s ^ "|\n"
+                  in
+    Lwt_io.(open_file ~flags:log_flags ~mode:output filename) >>= fun fch ->              
+    Lwt_io.write fch str >>= fun () ->
+    Lwt_io.close fch >>= fun () ->    
+    let msg = Parse_full_messages.parse_top_level_msg msg in 
+    write_seqn session_dir "seqin" msg >>= fun () ->  
+    do_timechange mailbox >>= fun () ->
+    Lwt_mvar.put mailbox (Fix_global_state.FIX_Message msg)
+  )
+;;
+
+let send_msg session_dir outch fmsg =
+  let filename = Filename.(concat session_dir "fix.log") in   
+  match fmsg with
+  | Full_messages.ValidMsg msg -> begin
+    let wire = Encode_full_messages.encode_full_valid_msg msg in
+    let log = String.map (function '\001' -> '|' | c -> c) wire ^ "\n" in
+    Lwt_io.(open_file ~flags:log_flags ~mode:output filename) >>= fun fch ->              
+    Lwt_io.write fch log                      >>= fun () ->
+    Lwt_io.close fch                          >>= fun () ->    
+    Lwt_io.write outch wire                   >>= fun () ->
+    Lwt_io.flush outch                        >>= fun () ->
+    write_seqn session_dir "seqout" fmsg 
+  end
+  | _ ->  Lwt.return_unit
+;;
+
+let create config (inch, outch) =
+  let hostid = match config.host_id with None -> "NoHostid" | Some host_id -> host_id in
+  let session_folder = Printf.sprintf "%s.%s.%s.session" config.comp_id config.target_id hostid in
+  let engine_state = make_engine_state session_folder config in
+  let initmsg = Fix_engine_state.(IncIntMsg_CreateSession { 
+    dest_comp_id = engine_state.fe_target_comp_id;
+    reset_seq_num = config.reset_seq
+  } ) in  
+  let mailbox, global_state = 
+    Fix_global_state.start 
+      ~pub:config.pub_callback 
+      engine_state 
+      State.init_model_state 
+      (send_msg session_folder outch) 
+  in {
+    heartbeat = heartbeat_thread mailbox;
+    fix = fix_thread session_folder inch mailbox;
+    mailbox = mailbox;
+    initmsg = initmsg
+  }
+;;  
