93f6c82ff80865ee482802a0753df0d2f6148647
Nicola Mometto <me@bronsa.net>
2018-02-20 17:25:25 +0000

    update :l to #mod_use

--- vgs/top.ml
+++ vgs/top.ml
@@ -8,23 +8,22 @@
     Toplevel model load file for Imandra.
 *)
 
-:l src-core/datetime.ml
-:l src-core/numeric.ml
-:l src-core/base_types.ml
+#mod_use "src-core/datetime.ml"
+#mod_use "src-core/numeric.ml"
+#mod_use "src-core/base_types.ml"
 
-:l src-protocol/full_admin_tags.ml
-:l src-protocol-exts-vg/full_app_tags.ml
-:l src-protocol/full_message_tags.ml
+#mod_use "src-protocol/full_admin_tags.ml"
+#mod_use "src-protocol-exts-vg/full_app_tags.ml"
+#mod_use "src-protocol/full_message_tags.ml"
 
-:l src-protocol/full_admin_enums.ml
-:l src-protocol/full_admin_messages.ml
-:l src-protocol-exts-vg/full_app_enums.ml
-:l src-protocol-exts-vg/full_app_records.ml
-:l src-protocol-exts-vg/full_app_messages.ml
+#mod_use "src-protocol/full_admin_enums.ml"
+#mod_use "src-protocol/full_admin_messages.ml"
+#mod_use "src-protocol-exts-vg/full_app_enums.ml"
+#mod_use "src-protocol-exts-vg/full_app_records.ml"
+#mod_use "src-protocol-exts-vg/full_app_messages.ml"
 
-:l src-protocol/full_messages.ml
-:l src/fix_engine_state.ml
-:l src/fix_engine_utils.ml
-:l src/fix_engine_transitions.ml
-:l src/fix_engine.ml
-
+#mod_use "src-protocol/full_messages.ml"
+#mod_use "src/fix_engine_state.ml"
+#mod_use "src/fix_engine_utils.ml"
+#mod_use "src/fix_engine_transitions.ml"
+#mod_use "src/fix_engine.ml"
