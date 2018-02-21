d53ce9b245cf6676de920de2ae9cb189dd052081
Nicola Mometto <me@bronsa.net>
2018-02-21 16:30:57 +0000

    datetime vgs

--- vgs/datetime.ml
+++ vgs/datetime.ml
@@ -1,53 +1,61 @@
 (* Basic Datetime VGs *)
 
-verify utc_gt_transitive (x,y,z) =
+let utc_gt_transitive (x,y,z) =
  utctimestamp_GreaterThan(x,y)
  && utctimestamp_GreaterThan(y,z)
  ==>
  utctimestamp_GreaterThan(x,z)
 ;;
 
-verify utc_gt_irreflexive (x) =
+Verify.top "utc_gt_transitive";;
+
+let utc_gt_irreflexive (x) =
  not(utctimestamp_GreaterThan(x,x))
 ;;
 
-verify utc_gt_total (x,y) =
+Verify.top "utc_gt_irreflexive";;
+
+let utc_gt_total (x,y) =
  x = y
  || utctimestamp_GreaterThan(x,y)
  || utctimestamp_GreaterThan(y,x)
 ;;
 
-verify normalise_id (x) =
+Verify.top "utc_gt_total";;
+
+let normalise_id (x) =
  is_valid_utctimestamp x
  ==>
  normalise_timestamp x = x
 ;;
 
-theorem[rw] float_LT_GT_elim (x,y) =
-  float_LessThan(x,y) ==> float_GreaterThan(x,y) = false
-;;
+Verify.top "normalise_id";;
 
-theorem[rw] float_GT_LT_elim (x,y) =
-  float_GreaterThan(x,y) ==> float_LessThan(x,y) = false
-;;
-
-theorem[rw] utctimeonly_GT_EQ_elim (x,y) =
-  utctimeonly_GreaterThan(x,y) ==> utctimeonly_Equal(x,y) = false
-;;
-
-theorem[rw] utctimeonly_GT_arith_elim_hour (x,y) =
-  x.utc_timeonly_hour > y.utc_timeonly_hour ==> utctimeonly_GreaterThan(x,y)
-;;
-
-theorem[rw] utctimeonly_GT_arith_elim_min (x,y) =
-  x.utc_timeonly_hour = y.utc_timeonly_hour
-  && x.utc_timeonly_minute > y.utc_timeonly_minute
-   ==> utctimeonly_GreaterThan(x,y)
-;;
-
-theorem[rw] utctimeonly_GT_arith_elim_sec (x,y) =
-  x.utc_timeonly_hour = y.utc_timeonly_hour
-  && x.utc_timeonly_minute = y.utc_timeonly_minute
-  && x.utc_timeonly_second > y.utc_timeonly_second
-   ==> utctimeonly_GreaterThan(x,y)
-;;
+(* theorem[rw] float_LT_GT_elim (x,y) =
+ *   float_LessThan(x,y) ==> float_GreaterThan(x,y) = false
+ * ;;
+ *
+ * theorem[rw] float_GT_LT_elim (x,y) =
+ *   float_GreaterThan(x,y) ==> float_LessThan(x,y) = false
+ * ;;
+ *
+ * theorem[rw] utctimeonly_GT_EQ_elim (x,y) =
+ *   utctimeonly_GreaterThan(x,y) ==> utctimeonly_Equal(x,y) = false
+ * ;;
+ *
+ * theorem[rw] utctimeonly_GT_arith_elim_hour (x,y) =
+ *   x.utc_timeonly_hour > y.utc_timeonly_hour ==> utctimeonly_GreaterThan(x,y)
+ * ;;
+ *
+ * theorem[rw] utctimeonly_GT_arith_elim_min (x,y) =
+ *   x.utc_timeonly_hour = y.utc_timeonly_hour
+ *   && x.utc_timeonly_minute > y.utc_timeonly_minute
+ *    ==> utctimeonly_GreaterThan(x,y)
+ * ;;
+ *
+ * theorem[rw] utctimeonly_GT_arith_elim_sec (x,y) =
+ *   x.utc_timeonly_hour = y.utc_timeonly_hour
+ *   && x.utc_timeonly_minute = y.utc_timeonly_minute
+ *   && x.utc_timeonly_second > y.utc_timeonly_second
+ *    ==> utctimeonly_GreaterThan(x,y)
+ * ;; *)
