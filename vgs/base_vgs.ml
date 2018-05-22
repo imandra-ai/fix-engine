b6780b965cda99d70a2d023c95af912ec2212d34
KKostya <kostya@aestheticintegration.com>
2018-05-22 09:41:26 +0200

    Loading model in Imandra pure

--- vgs/base_vgs.ml
+++ vgs/base_vgs.ml
@@ -1,7 +1,7 @@
 (**
 
     Aesthetic Integration Limited
-    Copyright (c) 2014 - 2017
+    Copyright (c) 2014 - 2018
 
     Verification goals: base vgs.
 
--- vgs/top.ml
+++ vgs/top.ml
@@ -1,30 +1,33 @@
 (**
 
     Aesthetic Integration Limited
-    Copyright (c) 2014 - 2017
+    Copyright (c) 2014 - 2018
 
     Implementation of the FIX 4.4 protocol.
 
     Toplevel model load file for Imandra.
 *)
 
-:l src-core/datetime.ml
-:l src-core/numeric.ml
-:l src-core/base_types.ml
+#validate false;;
 
-:l src-protocol/full_admin_tags.ml
-:l src-protocol-exts-vg/full_app_tags.ml
-:l src-protocol/full_message_tags.ml
-
-:l src-protocol/full_admin_enums.ml
-:l src-protocol/full_admin_messages.ml
-:l src-protocol-exts-vg/full_app_enums.ml
-:l src-protocol-exts-vg/full_app_records.ml
-:l src-protocol-exts-vg/full_app_messages.ml
-
-:l src-protocol/full_messages.ml
-:l src/fix_engine_state.ml
-:l src/fix_engine_utils.ml
-:l src/fix_engine_transitions.ml
-:l src/fix_engine.ml
+let src = 
+  [ "src-core/datetime.ml"
+  ; "src-core/numeric.ml"
+  ; "src-core/base_types.ml"
+  ; "src-protocol/full_admin_tags.ml"
+  ; "src-protocol-exts-vg/full_app_tags.ml"
+  ; "src-protocol/full_message_tags.ml"
+  ; "src-protocol/full_admin_enums.ml"
+  ; "src-protocol/full_admin_messages.ml"
+  ; "src-protocol-exts-vg/full_app_enums.ml"
+  ; "src-protocol-exts-vg/full_app_records.ml"
+  ; "src-protocol-exts-vg/full_app_messages.ml"
+  ; "src-protocol/full_messages.ml"
+  ; "src/fix_engine_state.ml"
+  ; "src/fix_engine_utils.ml"
+  ; "src/fix_engine_transitions.ml"
+  ; "src/fix_engine.ml"
+  ] in
+Caml.List.iter System.mod_use src
+;;
 
