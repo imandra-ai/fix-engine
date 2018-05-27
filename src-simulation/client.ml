654a422bee6fe24ce9f3d8783905f0977dd1e903
KKostya <kostya@aestheticintegration.com>
2018-05-27 09:53:57 +0200

    Update cpyright strings

--- src-simulation/client.ml
+++ src-simulation/client.ml
@@ -2,7 +2,7 @@
 (***
     
     Aesthetic Integration Limited
-    Copyright (c) 2014 - 2017
+    Copyright (c) 2014 - 2018
 
     client.ml
     
@@ -95,7 +95,7 @@ let run_client fixhost fixport config zmqpub zmqrep =
     (**)
     let () = [
         "(*********                   FIX Engine Client                           *********)";
-        "(*********  (c)Copyright Aesthetic Integration Limited., 2014 - 2017     *********)\n";
+        "(*********  (c)Copyright Aesthetic Integration Limited., 2014 - 2018     *********)\n";
         Printf.sprintf " - FIX client connecting to %s:%d" fixhost fixport;
         Printf.sprintf " - FIX session %s -> %s" config.Fix_connection.comp_id config.Fix_connection.target_id;
         Printf.sprintf " - Internal messages are published on ZMQ socket %s" zmqpub;
