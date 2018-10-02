55bfa93e1df684375cee55d82f30bb1ef2bd5cd0
Ewen Maclean <ewenmaclean@gmail.com>
2018-10-02 12:36:31 +0100

    finalising datetime json parsers

--- src-core-pp/datetime_json.ml
+++ src-core-pp/datetime_json.ml
@@ -101,7 +101,7 @@ let duration_to_json ( d : fix_duration ) =
   `Assoc list_assoc
 ;;
 
-let json_to_duration json = 
+let json_to_duration_opt json = 
   match JU.(json |> member "dur_years"   |> to_int_option ) with None -> None | Some dur_years    ->
   match JU.(json |> member "dur_months"  |> to_int_option ) with None -> None | Some dur_months   ->
   match JU.(json |> member "dur_days"    |> to_int_option ) with None -> None | Some dur_days     ->
@@ -131,6 +131,16 @@ let localmktdate_to_json ( d : fix_localmktdate ) =
   `Assoc list_assoc
 ;;
 
+let json_to_localmktdate_opt json = 
+  match JU.(json |> member "localmktdate_year"   |> to_int_option ) with None -> None | Some localmktdate_year    ->
+  match JU.(json |> member "localmktdate_month"  |> to_int_option ) with None -> None | Some localmktdate_month   ->
+  match JU.(json |> member "localmktdate_day"    |> to_int_option ) with None -> None | Some localmktdate_day     ->
+    Some { localmktdate_year;
+           localmktdate_month;
+           localmktdate_day
+         }
+;;
+
 let localmktdate_opt_to_json = function
   | None    -> `Null
   | Some x  -> localmktdate_to_json x
@@ -146,11 +156,20 @@ let utcdateonly_to_json ( d : fix_utcdateonly ) =
   `Assoc list_assoc
 ;;
 
+let json_to_utcdateonly_opt json = 
+  match JU.(json |> member "utc_dateonly_year"   |> to_int_option ) with None -> None | Some utc_dateonly_year    ->
+  match JU.(json |> member "utc_dateonly_month"  |> to_int_option ) with None -> None | Some utc_dateonly_month   ->
+  match JU.(json |> member "utc_dateonly_day"    |> to_int_option ) with None -> None | Some utc_dateonly_day     ->
+    Some { utc_dateonly_year;
+           utc_dateonly_month;
+           utc_dateonly_day
+         }
+;;
+
 let utcdateonly_opt_to_json = function  
   | None    -> `Null
   | Some x  -> utcdateonly_to_json x
 ;;
-
 
 (** UTC Timeonly *)
 let utctimeonly_milli_to_json ( d : fix_utctimeonly_milli ) = 
@@ -161,6 +180,18 @@ let utctimeonly_milli_to_json ( d : fix_utctimeonly_milli ) =
     ( "utc_timeonly_millisec" ,  int_opt_to_json d.utc_timeonly_millisec  ) 
   ] |> filter_nulls in
   `Assoc list_assoc
+;;
+
+let json_to_utctimeonly_milli_opt json = 
+  match JU.(json |> member "utc_timesonly_hour"   |> to_int_option ) with None -> None | Some utc_timeonly_hour    ->
+  match JU.(json |> member "utc_timesonly_minute" |> to_int_option ) with None -> None | Some utc_timeonly_minute  ->
+  match JU.(json |> member "utc_timesonly_second" |> to_int_option ) with None -> None | Some utc_timeonly_second  ->
+    let utc_timeonly_millisec = JU.(json |> member "utc_timeonly_millisec" |> to_int_option ) in 
+    Some { utc_timeonly_hour   
+         ; utc_timeonly_minute 
+         ; utc_timeonly_second 
+         ; utc_timeonly_millisec
+         }
 ;;
 
 let utctimeonly_milli_opt_to_json  = function
@@ -178,6 +209,18 @@ let utctimeonly_micro_to_json ( d : fix_utctimeonly_micro ) =
   `Assoc list_assoc
 ;;
 
+let json_to_utctimeonly_micro_opt json = 
+  match JU.(json |> member "utc_timesonly_hour"   |> to_int_option ) with None -> None | Some utc_timeonly_hour    ->
+  match JU.(json |> member "utc_timesonly_minute" |> to_int_option ) with None -> None | Some utc_timeonly_minute  ->
+  match JU.(json |> member "utc_timesonly_second" |> to_int_option ) with None -> None | Some utc_timeonly_second  ->
+    let utc_timeonly_microsec = JU.(json |> member "utc_timeonly_microsec" |> to_int_option ) in 
+    Some { utc_timeonly_hour   
+         ; utc_timeonly_minute 
+         ; utc_timeonly_second 
+         ; utc_timeonly_microsec
+         }
+;;
+
 let utctimeonly_micro_opt_to_json  = function
   | None    -> `Null
   | Some x  -> utctimeonly_micro_to_json x
@@ -189,6 +232,17 @@ let week_to_json = function
   | Week_3 -> `String "Week3"
   | Week_4 -> `String "Week4"
   | Week_5 -> `String "Week5"
+;;
+
+let json_to_week_opt json = 
+  match JU.(json |> to_string_option ) with None -> None | Some w ->
+    (match w with
+     | "Week1" -> Some Week_1
+     | "Week2" -> Some Week_2
+     | "Week3" -> Some Week_3
+     | "Week4" -> Some Week_4
+     | "Week5" -> Some Week_5
+     | _ -> None)
 ;;
 
 let week_opt_to_json = function
@@ -205,6 +259,18 @@ let monthyear_to_json ( d : fix_monthyear) =
     ( "monthyear_week"  , week_opt_to_json  d.monthyear_week  );
   ] |> filter_nulls in
   `Assoc list_assoc
+;;
+
+let json_to_monthyear_opt json = 
+  match JU.(json |> member "monthyear_year"   |> to_int_option ) with None -> None | Some monthyear_year    ->
+  match JU.(json |> member "monthyear_month"  |> to_int_option ) with None -> None | Some monthyear_month   ->
+    let monthyear_day =  JU.(json |> member "monthyear_day"    |> to_int_option ) in
+    let monthyear_week =  JU.(json |> member "monthyear_week"    |> json_to_week_opt )in
+    Some {monthyear_year;
+          monthyear_month;
+          monthyear_day;
+          monthyear_week
+         }
 ;;
 
 let monthyear_opt_to_json = function
