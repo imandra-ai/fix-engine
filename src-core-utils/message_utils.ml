375afa370988b7854d54754c5c4f1231c0820ebb
Ewen Maclean <ewenmaclean@gmail.com>
2018-11-30 12:15:10 +0000

    WIP: updating to iml with latest version

--- src-core-utils/message_utils.ml
+++
@@ -1,7 +0,0 @@
-open Model_messages
-
-let get_resend_message (msg:model_top_level_msg):model_top_level_msg = 
-  match msg with
-  | Model_messages.FIX_TL_Normal m -> Model_messages.FIX_TL_PossibleResend m 
-  | m -> m
-;;
