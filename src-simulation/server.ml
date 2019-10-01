276ece71a4b15287ae3438bb14967dfd50ee5568
Ewen Maclean <ewenmaclean@gmail.com>
2019-10-01 22:07:45 +0100

    server/client fixed - need to do the same for tests

---
+++ src-simulation/server.ml
@@ -0,0 +1,19 @@
+(** FIX 4.4 engine server implementation. *)
+(***
+    
+    Aesthetic Integration Limited
+    Copyright (c) 2014 - 2018
+
+    server.ml
+    
+*)
+
+let server_thread =
+    let addr = Caml_unix.( ADDR_INET( inet_addr_loopback , 9880 ) ) in
+    let _server = Lwt_io.establish_server_with_client_address addr Server_utils.f in
+    print_endline "Started server on localhost:9880 ...";
+    fst (Lwt.wait ())
+;;
+
+Lwt_main.run server_thread
+;;
