c4769363aaae15e8ead307c88e58c9fb452f3a07
KKostya <kostya@aestheticintegration.com>
2022-03-28 18:14:38 +0200

    Wip. Finished wiring server module

--- src-runner/server.ml
+++ src-runner/server.ml
@@ -1,61 +1,63 @@
 let (>>=) = Lwt.(>>=)
+let (let*) = Lwt.bind 
 
-type state =
+type direction = Incoming | Outgoing
+type msg_type = Application | Admin 
+
+type event = 
+  { message : (string * string) list
+  ; direction : direction
+  ; msg_type : msg_type
+  }
+
+type t =
   { engine_box : Engine.event Lwt_mvar.t
   ; fixio_box : Fix_io.message Lwt_mvar.t
   ; engine : Engine.t
   ; fixio : Fix_io.fix_io option
-  ; begin_string : string
-  ; timestamp_parse : string -> Datetime.fix_utctimestamp_micro option
-  ; timestamp_encode: Datetime.fix_utctimestamp_micro -> string
+  ; recv : event -> unit Lwt.t 
   }
 
-let log_fix_message pre msg =
-  let log = Fix_io.encode ~split:'|' msg in
-  Lwt_io.printlf "[%s]: %s" pre log
+let get_msg_type message =
+  match List.assoc_opt "35" message with 
+  | None -> Application | Some k -> 
+  match Parse_admin_tags.parse_admin_msg_tag k with
+  | None -> Application | Some _ -> Admin 
 
-let fixio_to_engine parse_timestamp engine msg =
-  log_fix_message "In " msg
-  >>= fun () ->
-  let msg = Parse_full_messages.parse_top_level_msg parse_timestamp msg in
-  Engine.send_fix engine msg >>= fun () -> Engine.send_getstate engine
-
-let engine_to_fixio cfg fixio msg =
-  let wire = Encode_full_messages.packet_full_valid_msg cfg msg in
-  log_fix_message "Out" wire >>= fun () -> Fix_io.send fixio wire
-
-let receive_engine (state:state) x = 
-  let engine_to_fixio =
-    engine_to_fixio Encode_full_messages.{ 
-      begin_string = state.begin_string ;
-      encode_UTCTimestamp = state.timestamp_encode
-    } in
-  match x with
-  | Engine.FIXMessage (Full_messages.ValidMsg msg) -> (
-    match state.fixio with 
-    | Some fixio -> engine_to_fixio fixio msg
-    | None -> Lwt.return_unit 
-    )
-| Engine.OutFIXData (_, msg) ->
-    Model.send_fix t.model (OutIntMsg_ApplicationData msg)
-| _ ->
-    Lwt.return_unit 
-
-
-let rec loop (state : state) =
-  Lwt.pick
-  [(* Lwt_mvar.take state.engine_box >>= *)
+let receive_fix_io t message =
+  let msg_type = get_msg_type message in
+  Lwt.join 
+  [ Engine.process_fix_wire t.engine message
+  ; t.recv { message ; direction = Incoming ; msg_type } 
   ]
 
+let receive_engine t event = 
+  match event, t.fixio with
+  | Engine.FIXFromEngine message , Some fixio -> begin
+    let msg_type = get_msg_type message in
+    Lwt.join 
+    [ Fix_io.send fixio message
+    ; t.recv { message ; direction = Outgoing ; msg_type } 
+    ]  
+  end
+  | _ -> Lwt.return_unit 
 
-let handler (state : state) (_in_addr: Unix.sockaddr) (inch, outch) =
+let loop (t : t) : unit Lwt.t =
+  let rec loop_box box receiver =
+    let* msg = Lwt_mvar.take box in
+    let* () = receiver t msg in 
+    loop_box box receiver
+    in
+  loop_box t.engine_box receive_engine
+  
+let handler (t : t) (_in_addr: Unix.sockaddr) (inch, outch) =
   (* FIX IO loop *)
-  let recv = Lwt_mvar.put state.fixio_box in
+  let recv = Lwt_mvar.put t.fixio_box in
   let fixio_thread, fixio = Fix_io.start ~recv (inch, outch) in
-  let state = {state with fixio = Some fixio } in
+  let t = {t with fixio = Some fixio } in
   Lwt.pick
   [ fixio_thread
-  ; loop state
+  ; loop t
   ]
 
 
@@ -81,29 +83,28 @@ let default_session_folder ~(config:Engine.config) =
 let start
     ?(session_dir : string option)
     ?(reset : bool option)
-    ?(begin_string: string option)
-    ?(millisecond_precision: bool option)
     ~(config : Engine.config)
     ~(port : int)
+    ~(recv : event -> unit Lwt.t )
     ()
   =
   let reset = match reset with None -> false | Some x -> x in
-  let begin_string = match begin_string with None -> "FIX.4.2" | Some x -> x in
   let session_dir = match session_dir with 
     | Some x -> x 
     | None -> default_session_folder ~config 
     in
-  let timestamp_parse, timestamp_encode = get_timestamp_codec millisecond_precision in
   let engine_box = Lwt_mvar.create_empty () in
   let fixio_box = Lwt_mvar.create_empty () in
-  let engine_thread , engine = Engine.start ~reset ~session_dir ~config in
+  let engine_thread , engine = 
+    let recv = Lwt_mvar.put engine_box in
+    Engine.start ~reset ~session_dir ~config ~recv 
+    in
   let state =
     { fixio = None
     ; engine
     ; engine_box
     ; fixio_box
-    ; begin_string
-    ; parse_timestamp
+    ; recv
     } in
   let addr = Unix.(ADDR_INET (inet_addr_loopback, port)) in
   let server_thread = 
