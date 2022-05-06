1f2e2754c9497e1e375db482ccc087459c1afcfb
KKostya <kostya@aestheticintegration.com>
2022-05-06 21:44:40 +0200

    Rename fix-engine-gen => model

--- model/sub.sh
+++ model/sub.sh
@@ -2,6 +2,6 @@ for f in $(find -name dune)
 do  
   for n in protocol_exts venue_types venue_strings_venue venue_helper_functions conversions venue types_pp string_factory core_time_defaults core_fresh protocol_exts_pp core_time_defaults_pp simulation_utils_gen
   do
-    sed -i "s/fix-engine.$n/fix-engine-gen.$n/g" $f
+    sed -i "s/fix-engine.$n/model.$n/g" $f
   done
 done
