357cf22c5e1f6abb68b8feb86f3cbfeadfcaf378
Nicola Mometto <brobronsa@gmail.com>
2022-11-17 15:52:30 -0600

    WIP: allow logon extra tags

--- fix-engine/src-runtime/config.ml
+++ fix-engine/src-runtime/config.ml
@@ -58,6 +58,9 @@ module Decode (D : Decoders.Decode.S) = struct
     let* reset = field_def "reset" false D.bool in
     let* no_history = field_def "no_history" false D.bool in
     let* heartbeat_interval = field_def "heartbeat_interval" 30 D.int in
+    let* logon_fields =
+      field_def "logon_fields" [] D.(list (D.pair string string))
+    in
     let engine_config =
       Engine.
         {
@@ -67,6 +70,7 @@ module Decode (D : Decoders.Decode.S) = struct
           on_behalf_id;
           timer;
           millisecond_precision;
+          logon_fields;
           next_expected_msg_seq_num = false;
           begin_string;
           ignore_business_reject = false;
