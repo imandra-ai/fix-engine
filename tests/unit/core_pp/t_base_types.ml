254175d8ddb7fce487419c08a02db2d7005dbe2e
Emma Tye <tyelouiseemma@gmail.com>
2023-11-22 10:13:03 +0000

    feat(core-pp): fix bug with parse_float, add parse_float unit tests

---
+++ tests/unit/core_pp/t_base_types.ml
@@ -0,0 +1,34 @@
+module Parse_float = struct
+  let float_multiplier = Float.of_int 1000000
+
+  let float_6_to_float float_6 =
+    match float_6 with
+    | Numeric.Float_6 i ->
+      Float.div (i |> Z.to_int |> Float.of_int) float_multiplier
+
+  let assert_parse_some_eq input expected =
+    let actual = Parse_base_types.parse_float input in
+    match actual with
+    | Some actual ->
+      let actual = float_6_to_float actual in
+      if actual <> expected then (
+        Printf.eprintf "parse_float: not equal - expected:%f vs actual:%f\n"
+          expected actual;
+        assert false
+      )
+    | None ->
+      Printf.eprintf "parse_float: float parse failure of: %s\n" input;
+      assert false
+
+  let () = assert_parse_some_eq "0" 0.
+
+  let () = assert_parse_some_eq "123" 123.
+
+  let () = assert_parse_some_eq "1234567" 1234567.
+
+  let () = assert_parse_some_eq "12.3456" 12.3456
+
+  let () = assert_parse_some_eq "0.123456" 0.123456
+
+  let () = assert_parse_some_eq "0.1234567" 0.123456
+end
