6f85f439dd78ad3f63c442cc02a6f4b391578291
Filippo Sestini <filippo@imandra.ai>
2024-08-13 10:39:47 +0100

    chore: add refs to message.mli

--- fix-engine/src-core-utils-msg/message.mli
+++ fix-engine/src-core-utils-msg/message.mli
@@ -2,6 +2,8 @@ type t = (string * string) list [@@deriving show]
 
 val checksum_string : string -> int
 
+val do_check_checksum : bool ref
+
 val valid_checksum : t -> bool
 (** Verifies the checksum (sum of all bytes mod 256) of all bytes in
     the message up to the CheckSum<10> entry. Returns true if the computed
@@ -10,6 +12,8 @@ val valid_checksum : t -> bool
 
 val checksum : t -> int
 (** A checksum in [[0..<256]] *)
+
+val do_check_body_length : bool ref
 
 val valid_body_length : t -> bool
 (** Checks that the message contains BodyLength<9> field as a second entry
